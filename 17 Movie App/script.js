d135a5706bf03492337cbb4215ce61741690afe1
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-11-20 21:16:36 +0530

    added js

--- 17 Movie App/script.js
+++ 17 Movie App/script.js
@@ -4,6 +4,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=102568db51f7574c4f738df5cf0cd2bd&query='
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -12,7 +13,43 @@ getMoviesData(API_URL)
 async function getMoviesData(url) {
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data.results)
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <img
+        src="${IMG_URL + poster_path}"
+        alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getVoteColor(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getVoteColor(value) {
+    if (value >= 8) {
+        return 'green'
+    } else if (value >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
